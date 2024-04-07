9c50bc3d890d811d99b70495a20f8546ef3e411f
karldreta <karldreta@gmail.com>
2024-04-08 02:40:51 +0800

    Add prompt button

--- script.js
+++ script.js
@@ -1,29 +1,44 @@
 const container = document.querySelector('#container');
+const body = document.querySelector('body');
+const button = document.createElement('button');
+button.textContent = "Choose number of squares"
+button.addEventListener('click', generateBoxes);
+button.id = 'button';
+body.appendChild(button);
 
-// Initialize the boxes via loop
-for (let i = 1; i <= 256; i++) {
 
-    const box = document.createElement('div');
-    const boxNum = document.createElement('p');
+function generateBoxes() {
+    container.innerHTML = '';
+    let boxQuantity = parseInt(prompt("Maximum number of 100: ")**2);
 
-    box.classList.add('box');
-    box.id = 'box-' + i;
+    boxQuantity = Math.min(boxQuantity, 10000);
 
-    // boxNum.textContent = i;
-    // boxNum.classList.add('boxNum');   
-    // box.appendChild(boxNum);
+    // Initialize the boxes via loop
+    for (let i = 1; i <= boxQuantity; i++) {
+
+        const box = document.createElement('div');
+        const boxNum = document.createElement('p');
     
-    // Hovering
+        box.classList.add('box');
+        box.id = 'box-' + i;
 
-    container.appendChild(box);
+    
+        boxNum.textContent = i;
+        boxNum.classList.add('boxNum');   
+        box.appendChild(boxNum);
+    
+        container.appendChild(box);
+    }
+
+        // Hovering
+    const boxes = document.querySelectorAll('.box');
+
+    boxes.forEach(box => box.addEventListener('mouseenter', changeColor));
+
+    function changeColor(event) {
+        const box = event.target;
+        box.style.backgroundColor = 'black';
+    }
 }
 
-// Hovering
-const boxes = document.querySelectorAll('.box');
-boxes.forEach(box => box.addEventListener('mouseenter', changeColor));
-
-function changeColor(event) {
-    const box = event.target;
-    box.style.backgroundColor = 'black';
-}
 
